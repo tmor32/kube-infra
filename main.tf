79e4a93c3c4b491936bdf8c6257444a05a09935a
tmor32 <143096341+tmor32@users.noreply.github.com>
2025-06-04 18:06:23 -0400

    updated subnet regions

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_vpc" "k8s_vpc" {
 resource "aws_subnet" "k8s_subnet_a" {
   vpc_id                  = aws_vpc.k8s_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -44,7 +44,7 @@ resource "aws_subnet" "k8s_subnet_a" {
 resource "aws_subnet" "k8s_subnet_b" {
   vpc_id                  = aws_vpc.k8s_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
 
   tags = {
